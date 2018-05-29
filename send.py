540d85575d6717e3f49b365b1d04803bfe9c99a5
HG <lhgaa@ajou.ac.kr>
2018-05-30 00:28:31 +0900

    로그스태시 연결빼고 완료

---
+++ send.py
@@ -0,0 +1,15 @@
+
+from time import sleep
+import time
+import requests
+import os
+
+
+
+url='http://127.0.0.1:8000/upload'
+files={'content': open('1_1527596108.23.jpg','rb')}
+image_name = '1_1527596108.23.jpg'
+param = {'image_name':image_name}
+r=requests.post(url,files=files,params=param)
+a= r.content
+print(a)
